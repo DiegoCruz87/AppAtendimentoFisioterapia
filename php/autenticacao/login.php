c7217bc168e4c1521d89fbc7d923d26292e7eb12
Rodric1606 <rodrigodsc.cardozo@gmail.com>
2021-03-25 22:24:24 -0300

    Iniciando projeto
    Compartilhando arquivos no github

---
+++ php/autenticacao/login.php
@@ -0,0 +1,36 @@
+<!--
+REGRAS DE LOGIN E LOGAUT
+TELA DE LOGIN
+
+
+---------------------------------------->
+<?
+//cÓDIGO PHP
+
+
+?>
+<!-------------------------------------------->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AppAtendimentoFisioterapiat</title>
+</head>
+<body>
+     <header>
+        <nav>
+
+        </nav>
+     </header>
+     <div>
+        <section>
+            Seja be vindo!
+        </section>
+     </div>
+
+
+</body>
+</html>
