2883a39b588feff3179af593868e2dbdaf1aa050
DiegoCruz87 <80112599+DiegoCruz87@users.noreply.github.com>
2021-04-09 19:03:34 -0300

    Novos Arquivos
    index.php
    conexao.php
    processa.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Cadastrar</title>
+	</head>
+	<body>
+		<h1>Cadastrar Usuário</h1>
+		<form method="POST" action="processa.php">
+			<label>Nome: </label>
+			<input type="text" name="nome" placeholder="Digite o nome completo"><br><br>
+
+			<label>E-mail: </label>
+			<input type="email" name="email" placeholder="Digite o seu email"><br><br>
+
+			<input type="submit" value="Cadastrar">
+
+		</form>
+	</body>
+</html>
